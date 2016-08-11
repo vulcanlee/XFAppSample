2abfc0b74a218e12d3d4b7e3a46504821383ea05
李進興 Vulcan <vulcan@miniasp.com>
2016-08-11 12:09:47 +0800

    開放資料跨平台應用程式開發 建立專案

---
+++ XFCreative/2.LoadList/XFCreative/XFCreative/XFCreative/Services/GlobalData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XFCreative.Repositories;
+
+namespace XFCreative.Services
+{
+    public class GlobalData
+    {
+        public static 創業空間Repository 創業空間Repository = new 創業空間Repository();
+    }
+}
