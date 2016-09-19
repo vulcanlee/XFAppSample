7b54d1d233fcbb1b1dc280acd48ee55791f63964
李進興 Vulcan <vulcan@miniasp.com>
2016-09-19 14:49:16 +0800

    企業之差旅費用 跨平台應用程式開發 - 系統初始化與使用者登入

---
+++ XFDoggy/3.InitLogin/XFDoggy/XFDoggy/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Unity;
+using XFDoggy.Views;
+
+namespace XFDoggy
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("LoadingPage");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+            Container.RegisterTypeForNavigation<LoadingPage>();
+            Container.RegisterTypeForNavigation<LoginPage>();
+        }
+    }
+}
